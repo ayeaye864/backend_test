31e76570d12d4efe4ff38033ea4671217950ab2e
Ayesha A <you@example.com>
2024-04-25 16:51:10 +0100

    updated pins

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at port: 3000"));
 app.use(express.static("public"));
-app.use(express.json({limit:"20mb"}));
+app.use(express.json({limit:"20mb"})); //prevents overloading server
 
-const db = new Datastore("database.db");
+const db = new Datastore("database.db"); //new database
 db.loadDatabase();
-db.insert({name: "Ana", subject: "biology", longitude: "53.45", latitude:"-2.2"});
-db.insert({name: "Jerry", subject: "english", longitude: "53.45", latitude:"-2.33"});
-db.insert({name: "Emily", subject: "mathematics", longitude: "53.40", latitude:"-2.35"});
-db.insert({name: "Rosco", subject: "engineering", longitude: "53.52", latitude:"-2.32"});
-db.insert({name: "Bob", subject: "fashion design", longitude: "53.50", latitude:"-2.08"});
+//insert data into the database
+// db.insert({name: "Ana", subject: "biology", longitude: "53.45", latitude:"-2.2"});
+// db.insert({name: "Jerry", subject: "english", longitude: "53.45", latitude:"-2.33"});
+// db.insert({name: "Emily", subject: "mathematics", longitude: "53.40", latitude:"-2.35"});
+// db.insert({name: "Rosco", subject: "engineering", longitude: "53.52", latitude:"-2.32"});
+// db.insert({name: "Bob", subject: "fashion design", longitude: "53.50", latitude:"-2.08"});
 
 // POST method route
 app.post('/api', (request, response) => {
@@ -20,6 +21,10 @@ app.post('/api', (request, response) => {
     console.log(myArray);
 
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    db.insert(data);
+
     myArray.push(data);
     console.log(data);
 
@@ -29,6 +34,17 @@ app.post('/api', (request, response) => {
         latitude:request.body.lat,
         longitude:request.body.lon,
     });
+});//takes 2 arguments, api enpdoint name+vaiables to receive and send request 
 
-    
-  });//takes 2 argiuments, api enpdoint name+vaiables to receive and send request 
+app.get('/api', (request, response) => {
+    db.find({}, (err,data)=>{
+    if (err){
+        response.end();
+        return;
+    } else{
+        console.log("Done");
+    }
+    response.json(data);
+    console.log(data);
+    });
+});
